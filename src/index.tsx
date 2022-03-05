92d3ea06d697a0fb6e519fccf75cb64e2a6cccb3
rohitkumarm <rohitkumarrk13568@gmail.com>
2022-03-05 15:41:14 +0530

    added default structure

---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+const Hello = <h1>Hello</h1>
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
